4bcccc645474e5e6832b643f1b74a7910cb55e62
Joseph Thomas <josephthomas88m@gmail.com>
2023-03-21 14:59:31 +0530

    Set opertaion updated

---
+++ datatype/set_remove_element.py
@@ -0,0 +1,12 @@
+myset={"a","b",'c','c','d','e','f','g','h','i','j','k'}
+
+myset.add("z")
+myset.update(['s','r','r','rfw'])
+print(myset)
+
+myset.remove('r')
+
+myset.discard('d')
+
+
+print(myset)
---
+++ datatype/setoperator.py
@@ -0,0 +1,39 @@
+
+
+set1={'a','b','c','d'}
+set2={'d','e','f','g'}
+
+# union
+
+# using union function
+print("Union operation using function: ")
+print(set1.union(set2))
+# using | opertator
+print("Union operation using operator: ")
+print(set1 | set2)
+
+# intersection
+
+# using function
+print("intersection operation usin function: ")
+print(set1.intersection(set2))
+# using & operator
+print("intersection operation using opertator: ")
+print(set1&set2)
+
+# DIFFERENCE
+# using 
+print("Difference operation using function: ")
+print(set1.difference(set2))
+
+# using - opertator
+print(set1-set2)
+
+# SYMATRIC DIFFERENCE
+# using function
+print("Symmentric difference operation using function: ")
+print(set1.symmetric_difference(set2))
+# using ^ operator
+print("Symmentric difference operation using operartor: ")
+print(set1 ^ set2)
+

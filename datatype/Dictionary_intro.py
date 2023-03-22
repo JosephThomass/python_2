5dde4fa13a5ea73ccb8473f18a442f03ee355dc6
Joseph Thomas <josephthomas88m@gmail.com>
2023-03-22 12:21:51 +0530

    Dictionary intro

---
+++ datatype/Dictionary_intro.py
@@ -0,0 +1,10 @@
+Employe = {"Name": "John", "Age": 24, "salary":75000,"Company":"X-Team"}      
+print(type(Employe))      
+print("printing Employee data .... ")      
+print(Employe)      
+
+Employee = {"Name": "David", "Age": 30, "salary":55000,"Company":"GOOGLE"}    
+print(type(Employee))    
+print("printing Employee data .... ")    
+print("Name : %s" %Employee["Name"])    
+print("Age : %d" %Employee["salary"])
